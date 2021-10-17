93f4cfee57f67dc7dda3ff077f64465ad4128a62
hardik9577 <hardik9577@gmail.com>
2021-10-17 20:28:58 +0530

    test1

---
+++ src/com/app/string/StringCalculator.java
@@ -0,0 +1,38 @@
+package com.app.string;
+
+public class StringCalculator {
+	
+    public int add(String input) {
+        String[] numbers = input.split(",|\n|;");
+
+        if (input.isEmpty()) {
+            return 0;
+        } else if (numbers.length > 1) {
+            return getSum(numbers);
+        }
+        return stringToInt(input);
+    }
+
+    private int getSum(String[] numbers) {
+        int sum = 0;
+        for (String currentNumber:numbers) {
+            if (stringToInt(currentNumber) > 1000) {
+                continue;
+            }
+            sum += stringToInt(currentNumber);
+        }
+        System.out.println("Sum = "+sum);
+        return sum;
+    }
+
+    private int stringToInt(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+        	System.out.println(num);
+            throw new IllegalArgumentException("Negative Not Allowed! ");
+        } else {
+            return num;
+        }
+    }
+
+}
---
+++ src/com/app/test/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.app.test;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.app.string.StringCalculator;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+public class CalculatorTest {
+	
+	
+
+	    private StringCalculator calculator;
+
+	    @Rule
+	    public ExpectedException thrown = ExpectedException.none();
+
+	    @Before
+	    public void initialize() {
+	        calculator = new StringCalculator();
+	    }
+
+	    //Empty String Should Return 0
+	    @Test
+	    public void test1_1() {
+	        assertEquals(calculator.add(""), 0);
+	    }
+	    
+	}
+
+
+
